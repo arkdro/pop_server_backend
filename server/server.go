65ac8588a40262e8f81c00d1987d5b1c76c3a597
Arkadiy Drovosekov <arkdro@gmail.com>
2017-07-19 00:00:03 +0200

    add server stub

--- server/server.go
+++ server/server.go
@@ -1,6 +1,24 @@
 package server
 
+import (
+	"log"
+	"net/http"
+)
+
 func Run(db Db_data) {
-	
+	handler := Handler{}
+	http.Handle("/point", handler)
+	http.HandleFunc("/countries",  get_countries)
+	res := http.ListenAndServe(":8080", nil)
+	log.Fatal(res)
+}
+
+type Handler struct {
+}
+
+func (h Handler) ServeHTTP(writer http.ResponseWriter, req *http.Request) {
+}
+
+func get_countries(resp http.ResponseWriter, req *http.Request) {
 }
 
